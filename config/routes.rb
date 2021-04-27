e7e8eea68a8664fa7e7f5ab72f56bc1ee716f47a
fersoftware <fersoftware.com>
2021-04-27 19:52:18 -0300

    Atualização CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :products, only: [:index, :new, :create]
+  resources :restaurants
+  devise_for :users
+  root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
